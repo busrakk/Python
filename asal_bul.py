aafff1c19f81afc711500d8409df1afdd28bb165
Büşra Kartal <62840507+busrakk@users.noreply.github.com>
2020-07-23 12:23:05 +0300

    Update asal_bul.py

--- asal_bul.py
+++ asal_bul.py
@@ -1,4 +1,4 @@
-
+#Fonksiyon kısmı
 def asal_bul(sayi):
     
     if sayi == 1:
@@ -8,11 +8,13 @@ def asal_bul(sayi):
         return True
     
     else:
+        #2 den girilen sayiya kadar..
         for i in range(2 , sayi):
             if sayi % i == 0:
                 return False
         return True
-    
+
+#Döngü doğru olduğu sürece devam eder    
 while True:
     sayi = int(input("Bir Sayı Giriniz : "))
     
@@ -21,3 +23,6 @@ while True:
         
     else:
         print(sayi, "Asal Sayı Değildir.")
+
+        
+        

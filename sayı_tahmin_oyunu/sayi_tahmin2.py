b0a54128a66111d700ef38c3976dc23df3cbd269
Büşra Kartal <62840507+busrakk@users.noreply.github.com>
2020-08-18 22:58:36 +0300

    Add files via upload

---
+++ sayı_tahmin_oyunu/sayi_tahmin2.py
@@ -0,0 +1,29 @@
+
+print('---- SAYI TAHMİN OYUNU -----')
+import random
+
+sayi = random.randint(1,100)
+can = int(input('Kaç hak kullanmak istersiniz : '))
+hak = can
+sayac = 0
+
+while hak > 0:
+    hak-=1
+    sayac += 1
+    tahmin = int(input('Tahmin : '))
+    
+    if sayi == tahmin:
+        print(f'Tebrikler {sayac} defada bildiniz.\nToplam puanınız : {100 -(100/can) * (sayac-1) }')
+        break
+    
+    elif sayi > tahmin:
+        print('Yukarı tahmin')
+        
+    else:
+        print('Aşağı tahmin')
+        
+    if hak == 0:
+        print(f'Hakkınız bitti. Tutulan sayı : {sayi} ')
+        
+
+

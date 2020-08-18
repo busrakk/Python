26504357911bbbf63ce63d2cb6df1d0b1339c94a
Büşra Kartal <62840507+busrakk@users.noreply.github.com>
2020-08-18 12:34:36 +0300

    Add files via upload

---
+++ urun_list.py
@@ -0,0 +1,22 @@
+
+print(' *** Ürün Listesi Oluşturma ***')
+urunler  = []
+sayac = 0
+
+adet = int(input('Kaç ürün eklemek istiyorsunuz :'))
+
+while(sayac < adet):
+    name = input('ürün ismi : ')
+    price = input('ürün fiyatı : ')
+    
+    #dictionary listesi 
+    urunler.append({
+        
+        'name' : name,
+        'price' : price
+    })
+    
+    sayac = sayac + 1
+
+for urun in urunler :
+    print(f'ürün adı : {urun["name"]} ürün fiyatı : {urun["price"]}')

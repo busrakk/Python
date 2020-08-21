63978dc6bd314980a6aab0c24cb9b4f7c0e80c03
Büşra Kartal <62840507+busrakk@users.noreply.github.com>
2020-08-21 21:41:51 +0300

    Add files via upload
    Bir sayının tam bölenlerini bulma

---
+++ tam_bolen_bul.py
@@ -0,0 +1,15 @@
+# tam bölen bulma
+
+def tamBolenleriBul(sayi):
+    tamBolenler = []
+    
+    for i in range(2, sayi):
+        if(sayi % i == 0):
+            tamBolenler.append(i)
+            
+    return tamBolenler
+
+sayi = int(input('sayi : '))
+
+print(tamBolenleriBul(sayi))
+
